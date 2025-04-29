30ffdbe1ae3404485e518fc6894c356e5d6968dd
Michael Santos <michael4lves.pro@gmail.com>
2025-04-29 08:28:14 -0300

    Criando o Primeiro Script
    Criação do script inicial para localização e a envio de informações para a página Web.

---
+++ Script/gfScraping.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import pandas as pd
+import time
+
+def clicar(driver, e_type, selector):
+    while True:
+        try:
+            driver.find_element(e_type, selector).click()
+        except:
+            pass
+        else:
+            break 
+
+partida_User = input("Digite a sua cidade de partida: ").strip().title()
+destino_User = input("Digite as cidades que você deseja, separado por vírgulas: ")
+data_ida = input("Digite a Data de Partida DD/MM/AA¨: ").strip()
+data_volta = input("Digite a Data de Retorn DD/MM/AA: ").strip()
+
+
+lista_cidade = [cidade.strip().title() for cidade in destino_User.split(",")]
+lista_info = []
+
+print(lista_cidade)
+driver = webdriver.Firefox()
+
+driver.get('https://www.google.com/travel/flights?gl=BR&hl=pt-BR')
+
+partida = driver.find_element(By.XPATH, '//input[@aria-label="De onde?"]')
+
+partida.send_keys(Keys.CONTROL + "a")  # Seleciona tudo
+partida.send_keys(Keys.BACKSPACE)      # Apaga
+partida.send_keys(partida_User)
+
+time.sleep(5)
+
+clicar(driver, By.XPATH, f'//li[@aria-label="{partida_User}"]')
+
+destino = driver.find_element(By.XPATH, '//input[@aria-label="Para onde? "]')
+destino.send_keys(lista_cidade)
+
+time.sleep(5)
+
+clicar(driver, By.XPATH, f"(//div[@class='zsRT0d'][normalize-space()='{lista_cidade[0]}'])[1]")
+
+data_partida = driver.find_element(By.XPATH, '//input[@aria-label="Partida"]')
+data_partida.click()
+time.sleep(5)
+
+input_partida = driver.find_element(By.XPATH, '//input[@aria-label="Partida"]')
+input_partida.send_keys(data_ida)
